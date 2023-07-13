74255ee10f031312a2095ebd80ef53a87d1fe6c2
Gabriel Soares <gabrsoares@outlook.com>
2023-07-13 17:26:19 -0300

    add form validation

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -4,6 +4,8 @@ import './Form.css'
 const Form = ({onInputChange}) => {
     const [firstInput, setFirstInput] = useState('')
     const [lastInput, setLastInput] = useState('')
+    const [isEmpty, setIsEmpty] = useState(true)
+    const [isSubmitted, setIsSubmitted] = useState(false)
 
     const handleInputChange = (event) => {
         const {id, value} = event.target
@@ -12,7 +14,16 @@ const Form = ({onInputChange}) => {
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        onInputChange(firstInput, lastInput)
+        setIsSubmitted(true)
+
+        if (firstInput !== '' && lastInput !== ''){
+            onInputChange(firstInput, lastInput)
+            setIsEmpty(false)
+            setFirstInput('')
+            setLastInput('')
+        } else {
+            setIsEmpty(true)
+        }
     }
 
     return (
@@ -21,6 +32,7 @@ const Form = ({onInputChange}) => {
                 <input type="text" id='first' value={firstInput} onChange={handleInputChange}/>
                 <input type="text" id='last' value={lastInput} onChange={handleInputChange}/>
             </div>
+            {isSubmitted && isEmpty? <p>Preencha todos os campos!</p> : ''}
             <button type='submit'>Enviar</button>
         </form>
     )
